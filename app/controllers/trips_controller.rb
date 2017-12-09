c0b18a5830b0fa1d915eb0b2aa4a37398a59aa4b
Erica Plofsky <ericaplofsky@gmail.com>
2017-12-09 19:44:20 +0000

    added routes for new trip and created new trip form

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,25 @@
 class TripsController < ApplicationController
+
+  get '/trips/new' do
+   if logged_in?
+    @user = current_user
+    erb :'/trips/new'
+   else
+    redirect "/login"
+  end
+ end
+
+ post '/trips' do
+  @user = current_user
+  if params[:date] == ""
+   redirect "/trips/new"
+  else
+   @trip = Trip.create(name: params[:name], location: params[:location], site: params[:site], date: params[:date], summary: params[:summary])
+   @trip.user_id = @user.id
+   @trip.save
+   redirect "/trips/#{@trip.id}"
+  end
+ end
+
+ 
 end
